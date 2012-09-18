2036b3a5218506f1556ac6427647ea58dd3d237f
Brandon Hansen <brandonh@ibethel.org>
2012-09-18 12:01:42 -0700

    Adding some seed data to make it easier to get up and running

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ ruby '1.9.3'
 gem 'rails', '3.2.7'
 gem 'pg'
 gem 'thin'
+gem 'forgery'
 
 gem 'redcarpet'
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+1.upto(10) do |n|
+  episode = Episode.create({
+    title: Forgery(:name).company_name,
+    summary: Forgery(:lorem_ipsum).sentences(1),
+    notes: Forgery(:lorem_ipsum).sentences(20),
+    published_at: n.weeks.ago,
+    seconds: Random.new.rand(1000...100000),
+    position: n
+  })
+  
+  [["video-720", "video/mpeg"], ["video-480", "video/mpeg"], ["audio-m4a", "audio/mpeg"]].each do |type|
+    EpisodeFile.create({
+      episode_id: episode.id,
+      file_type: type.first,
+      content_type: type.last,
+      size: Random.new.rand(10000...100000),
+      url: Forgery(:internet).domain_name
+    })
+  end
+  
+end
